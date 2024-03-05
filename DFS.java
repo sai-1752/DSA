2c80eb01a5a461c512a197f3802a257e7f866e7e
sai-1752 <94359408+sai-1752@users.noreply.github.com>
2024-03-05 10:42:15 +0530

    Add files via upload

---
+++ DFS.java
@@ -0,0 +1,69 @@
+
+import java.util.*;
+
+
+public class DFS {
+    static class Edge{
+        int src;
+        int des;
+
+        public Edge(int src,int des){
+            this.src=src;
+            this.des=des;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i]=new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0,1)); 
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0)); 
+        graph[1].add(new Edge(1,3)); 
+        
+        graph[2].add(new Edge(2,0)); 
+        graph[2].add(new Edge(2,4)); 
+
+        graph[3].add(new Edge(3,1)); 
+        graph[3].add(new Edge(3,4)); 
+        graph[3].add(new Edge(3,5));
+
+        graph[4].add(new Edge(4,2)); 
+        graph[4].add(new Edge(4,3)); 
+        graph[4].add(new Edge(4,5)); 
+
+        graph[5].add(new Edge(5,3)); 
+        graph[5].add(new Edge(5,4)); 
+        graph[5].add(new Edge(5,6)); 
+
+
+        graph[5].add(new Edge(6,5));
+
+        
+    
+    }
+
+    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[]){
+        System.out.println(curr + " ");
+        vis[curr]=true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e=graph[curr].get(i);
+            if (vis[e.des]==false) {
+                dfs(graph,e.des,vis);
+            }
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        int ver=7;
+        ArrayList<Edge> graph[]=new ArrayList[ver];
+        createGraph(graph);
+        boolean vis[]=new boolean[ver];
+        dfs(graph,0,vis);
+        System.out.println();
+    }
+}
